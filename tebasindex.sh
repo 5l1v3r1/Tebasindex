3bedade5e36c83d7016adb9e0ec65f13dd698b08
Step By Step <52529306+stepbystepexe@users.noreply.github.com>
2019-12-08 12:01:11 +0700

    Update and rename index.sh to tebasindex.sh

---
+++ tebasindex.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/bash
+# Tebas Index
+# Coded by Senja
+# Github: github.com/stepbystep/Tebasindex
+
+checkroot() {
+
+if [[ "$(id -u)" -ne 0 ]]; then
+    printf "\e[0m[\e[1;91m!\e[0m] \e[1;77mPlease, run this program as root!\n\n\e[0m"
+    exit 1
+fi
+
+}
+
+clearscreen() {
+
+clear
+reset
+sleep 1
+
+}
+
+dependencies() {
+
+command -v bash > /dev/null 2>&1 || { echo >&2 "I require bash but it's not installed. Install it. Aborting."; exit 1; }
+command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it. Aborting."; exit 1; }
+command -v openssl > /dev/null 2>&1 || { echo >&2 "I require openssl but it's not installed. Install it. Aborting."; exit 1; }
+
+}
+
+banner() {
+
+printf "\e[0;33m
+
+▄▄▄▄▄▄▄▄ .▄▄▄▄·  ▄▄▄· .▄▄ ·  ▄▄▄▄▄ ▐ ▄ ·▄▄▄▄  ▄▄▄ .▐▄• ▄
+•██  ▀▄.▀·▐█ ▀█▪▐█ ▀█ ▐█ ▀.  •██  •█▌▐███▪ ██ ▀▄.▀· █▌█▌▪
+ ▐█.▪▐▀▀▪▄▐█▀▀█▄▄█▀▀█ ▄▀▀▀█▄  ▐█.▪▐█▐▐▌▐█· ▐█▌▐▀▀▪▄ ·██·
+ ▐█▌·▐█▄▄▌██▄▪▐█▐█ ▪▐▌▐█▄▪▐█  ▐█▌·██▐█▌██. ██ ▐█▄▄▌▪▐█·█▌
+ ▀▀▀  ▀▀▀ ·▀▀▀▀  ▀  ▀  ▀▀▀▀   ▀▀▀ ▀▀ █▪▀▀▀▀▀•  ▀▀▀ •▀▀ ▀▀
+
+"
+
+}
+
+template() {
+
+printf "\e[0;45;37;1m:         Tebas Indexs v1.0, Coded by @stepbystep        :\e[0m\n"
+sleep 1
+printf "\n"
+
+
+}
+
+ready() {
+
+read -p $'\e[0m[\e[1;95m?\e[0m\e[0m] \e[1;77mWeb Target: \e[0m' link
+read -p $'\e[0m[\e[1;92m+\e[0m\e[0m] \e[1;77mInput Script: \e[0m' script
+
+sleep 1.5
+printf "\n\e[0m[\e[1;91m!\e[0m] \e[1;77mChecking ...\n"
+echo
+sleep 3
+
+curl -T /storage/emulated/0/ $script $link
+
+printf "\n\e[0m[\e[1;96m#\e[0m] \e[1;77mPlease see the link. \n\e[0m"
+echo
+exit 1
+echo
+
+}
+
+#checkroot
+#dependencies
+clearscreen
+banner
+template
+ready
