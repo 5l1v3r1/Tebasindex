73d315b60a23a42c96a8cfa08f98039769b6ceba
Step By Step <52529306+stepbystepexe@users.noreply.github.com>
2020-03-23 15:53:12 +0700

    Update tebasindex.sh

--- tebasindex.sh
+++ tebasindex.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 # Tebasindex
 # Coded by Nedi Senja
 # Github: https://github.com/stepbystep/Tebasindex
@@ -139,7 +139,7 @@ NB          : Manusia gax ada yang sempurna
               sama kaya tool ini.
               Silahkan laporkan kritik atau saran
               Ke - Email: d_q16x@outlook.co.id
-                 - WhatsApp: +62 8577-5433-901
+                 - WhatsApp: https://tinyurl.com/wel4alo
 
 [ \e[4mGunakan tool ini dengan bijak \e[0m]\n"
 sleep 1
